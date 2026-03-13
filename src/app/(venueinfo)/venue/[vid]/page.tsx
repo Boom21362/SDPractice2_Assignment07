3ed4caa7840982622c6fe7ac7cbf01f0ae588da8
Napat Putsorn <napatput31@hotmail.com>
2026-03-14 01:36:34 +0700

    Updated venue page (again)

--- src/app/(venueinfo)/venue/[vid]/page.tsx
+++ src/app/(venueinfo)/venue/[vid]/page.tsx
@@ -1,34 +1,38 @@
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
-export default async function venueDetailPage({params}:{params: Promise<{vid:string}>}){
-    /**
-     * Mock Data
-     */
-
-    const {vid} = await params;
+export default async function VenueDetailPage({ params }: { params: Promise<{ vid: string }> }) {
+    
+    const { vid } = await params;
 
     const mockVenueRepo = new Map();
-    mockVenueRepo.set("001", { name: "The Bloom Pavilion", image: "/img/bloom.jpg" })
-    mockVenueRepo.set("002", { name: "Spark Space", image: "/img/sparkspace.jpg" })
-    mockVenueRepo.set("003", { name: "The Grand Table", image: "/img/grandtable.jpg" })
+    mockVenueRepo.set("001", { name: "The Bloom Pavilion", image: "/img/bloom.jpg" });
+    mockVenueRepo.set("002", { name: "Spark Space", image: "/img/sparkspace.jpg" });
+    mockVenueRepo.set("003", { name: "The Grand Table", image: "/img/grandtable.jpg" });
 
     const venueItem = mockVenueRepo.get(vid);
 
-    return(
+    if (!venueItem) {
+        notFound();
+    }
+
+    return (
         <main className="text-center p-5">
+            <h1 className="text-lg font-medium"> Venue ID {vid} </h1>
             <div className="flex flex-row my-5">
-                <Image src={venueItem.image}
-                    alt = 'Venue Image'   
-                    width={0} height={0} sizes="100vw"
-                    className="rounded-lg w-[30%]"/>
-                <div className="text-md  mx-5"> {venueItem.name} </div>
+                <Image 
+                    src={venueItem.image}
+                    alt={venueItem.name}   
+                    width={400} 
+                    height={300}
+                    className="rounded-lg w-[30%]"
+                />
+                <div className="text-md mx-5"> {venueItem.name} </div>
             </div>
         </main>
     );
 }
 
-export async function generateStaticParams(){
-    return [{vid:'001'},{vid:'002'},{vid:'003'}]
-
+export async function generateStaticParams() {
+    return [{ vid: '001' }, { vid: '002' }, { vid: '003' }];
 }
